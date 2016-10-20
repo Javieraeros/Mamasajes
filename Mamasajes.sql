e03f4737a4232b28caf80a0da0c1283f99c2aeee
Javieraeros <Pajarrurro@gmail.com>
2016-10-20 22:17:23 +0200

    Cambios en la base de datos

--- Mamasajes.sql
+++ Mamasajes.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.0.10deb1
+-- version 4.5.2
 -- http://www.phpmyadmin.net
 --
 -- Servidor: localhost
--- Tiempo de generación: 13-10-2016 a las 04:08:18
--- Versión del servidor: 5.5.38-0ubuntu0.14.04.1
--- Versión de PHP: 5.5.9-1ubuntu4.5
+-- Tiempo de generación: 20-10-2016 a las 22:11:23
+-- Versión del servidor: 10.1.16-MariaDB
+-- Versión de PHP: 7.0.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,7 +14,7 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Base de datos: `Mamasajes`
@@ -25,16 +25,13 @@ SET time_zone = "+00:00";
 --
 -- Estructura de tabla para la tabla `Mamasajistas`
 --
--- Creación: 13-10-2016 a las 07:02:21
---
 
-CREATE TABLE IF NOT EXISTS `Mamasajistas` (
+CREATE TABLE `Mamasajistas` (
   `DNI` varchar(9) NOT NULL,
   `Nombre` varchar(30) NOT NULL,
   `Sexo` char(1) NOT NULL,
   `Edad` tinyint(11) NOT NULL,
-  `Mamasajes` char(1) NOT NULL DEFAULT 'N',
-  PRIMARY KEY (`DNI`)
+  `Mamasajes` varchar(1) NOT NULL DEFAULT 'N'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -42,6 +39,7 @@ CREATE TABLE IF NOT EXISTS `Mamasajistas` (
 --
 
 INSERT INTO `Mamasajistas` (`DNI`, `Nombre`, `Sexo`, `Edad`, `Mamasajes`) VALUES
+('', 'sdfgds', '', 0, ''),
 ('0000002B', 'Cayetana de Alba Folclorica', 'M', 45, 'N'),
 ('1A', 'Jose Carlos de Bourbon Vino', 'H', 99, 'N'),
 ('21548769B', 'Agapito Juan MAteos Lopez', 'H', 64, 'S'),
@@ -49,6 +47,17 @@ INSERT INTO `Mamasajistas` (`DNI`, `Nombre`, `Sexo`, `Edad`, `Mamasajes`) VALUES
 ('47123345G', 'Pepita de los Palotes ', 'M', 25, 'S'),
 ('47428156G', 'Francisca Ruiz Rodriguez', 'H', 35, 'S'),
 ('47428156H', 'Francisca Ruiz', 'H', 35, '0');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `Mamasajistas`
+--
+ALTER TABLE `Mamasajistas`
+  ADD PRIMARY KEY (`DNI`),
+  ADD UNIQUE KEY `DNI` (`DNI`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
